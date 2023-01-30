704f78540b40007072990676d6ae85f2a09e71c1
MariusAuerbacher <101823468+MariusAuerbacher@users.noreply.github.com>
2023-01-30 16:49:16 +0100

    first react homework

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import ButtonComponent from "./ButtonComponent";
+import ImageComponent from "./ImageComponent";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+      <ImageComponent
+src="https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+alt="unsplash laptop image"
+/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ButtonComponent label="this is a button" />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +24,11 @@ function App() {
         >
           Learn React
         </a>
+  
       </header>
     </div>
+
   );
-}
+};
 
 export default App;
